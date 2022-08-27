88b17068ea3849306f11c42602fa48fea2020eb5
Aishwarya <aishwarya.sivakumar48@gmail.com>
2022-08-27 12:58:39 +0530

    changes

---
+++ S06SpringJdbc/src/main/java/com/samples/S06SpringJdbc/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.samples.S06SpringJdbc.dao;
+
+import com.samples.S06SpringJdbc.dto.Employee;
+
+public interface EmployeeDAO {
+
+	int create(Employee employee);
+	
+	int update(Employee employee);
+	
+	int delete(int id);
+	
+	Employee read(int id);
+	
+}
